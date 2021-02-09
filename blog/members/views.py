7ef18a705fda571b44410c2832587dd3841c3388
Avyukth Nilajagi <73862038+AvyukthN@users.noreply.github.com>
2021-02-08 23:11:42 -0500

    Delete blog directory

--- blog/members/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-
-class UserRegisterView(generic.CreateView):
-    success_url = reverse_lazy('login')
